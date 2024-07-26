4b7cda03abb3ce57c8c1783442994bcf42837452
bg-miles <piuschukwuebuka40@gmail>
2024-07-26 23:45:59 +0100

    JS Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
-const myPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('foo');
-	}, 1000);
-});
-
-myPromise
-	.then(handleFulfilledA, handleRejectedA)
-	.then(handleFulfilledB, handleRejectedB)
-	.then(handleFulfilledC, handleRejectedC);
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
+}
