2d7a5307629ec4fc194a4e803cc53c119213a718
bg-miles <piuschukwuebuka40@gmail>
2024-08-03 01:16:55 +0100

    tab

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,3 @@
-export default function getListStudentIds (array) {
-  return Array.isArray(array) ? array.map((arr) => arr.id) : [];
+export default function getListStudentIds(array) {
+	return Array.isArray(array) ? array.map((arr) => arr.id) : [];
 }
